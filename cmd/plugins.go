ba5636ee9423944fd736b985a84b21a4e5741ec7
Filipe Pina <hzlu1ot0@duck.com>
2025-12-02 10:13:55 +0000

    x

--- cmd/plugins.go
+++ cmd/plugins.go
@@ -106,15 +106,6 @@ func (p *Plugins) DiscoverPlugins() error {
 		p.Executors[pluginName] = raw.(dkplugin.Executor)
 	}
 
-	// Load the embeded plugins
-	for _, pluginName := range embededPlugins {
-		raw, err := p.pluginFactory(exePath, []string{pluginName}, dkplugin.ExecutorPluginName)
-		if err != nil {
-			return err
-		}
-		p.Executors[pluginName] = raw.(dkplugin.Executor)
-	}
-
 	raw, err := p.pluginFactory(exePath, []string{"shell"}, dkplugin.ExecutorPluginName)
 	if err != nil {
 		return err
